53b1b069bcc913d725749bacb4a8007bd7326c99
Tihom007 <130473417+Tihom007@users.noreply.github.com>
2023-10-25 09:42:41 +0000

    data_types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "Tihom";
+let age = 23;
+let isLoggedIn = false;
+
+// number => 2 , 3 , 4 ,.....
+// string => "Hello world" ,  "2", "it is a string" ,........
+// boolean => true/false
+// null => It is a dataype as well as a standalone value
+// undefined => unassigned value
+// symbol => unique value 
+
+// object
+
+console.log(typeof undefined) // =>undefined
+console.log(typeof null); // => object 
