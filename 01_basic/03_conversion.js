733f8685294b4cb538fed8e690f92a7264c43ca2
Tihom007 <130473417+Tihom007@users.noreply.github.com>
2023-10-25 10:06:55 +0000

    Conversion of data_types

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,12 @@
+let life = "3";
+
+console.log(typeof life);
+console.log(typeof(life));
+
+let lifeInNumber = Number(life);
+
+console.log(typeof lifeInNumber); // => number
+
+// '33' => 33
+// true = 1  , false = 0
+// 'Tihom' => Nan (Not a number)
