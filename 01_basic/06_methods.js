f016fde20203b207c5e621f903eef88b45bf04de
Tihom007 <130473417+Tihom007@users.noreply.github.com>
2023-10-25 19:22:37 +0000

    method in string & number

---
+++ 01_basic/06_methods.js
@@ -0,0 +1,28 @@
+// Methods In String
+
+let myName = "Tihom ramuk";
+
+// console.log(myName.toLocaleUpperCase());
+// console.log(myName.charAt(4));
+// console.log(myName.includes('k'));
+// console.log(myName.slice(3,8))
+// console.log(myName.length)
+// console.log(myName.replace("Tihom", "Mohit"))
+// console.log(myName.split(" "))
+
+// Methods in Number
+
+let myNumber = 765.908999;
+// console.log(myNumber.toString().length);
+// console.log(myNumber.toFixed(2))
+
+// Math method in javaScript
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()* 100));
+let min = 1;
+let max = 6;
+
+console.log(Math.floor(Math.random() * (max- min + 1)) + min );
+
+
