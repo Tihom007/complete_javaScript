586f19bb25482305cbc94092aa0bfc5d35b4b620
Tihom007 <130473417+Tihom007@users.noreply.github.com>
2023-10-26 07:23:25 +0000

    date & time

---
+++ 01_basic/07_Date-Time.js
@@ -0,0 +1,14 @@
+const date = new Date();
+// console.log(date.toLocaleString());
+// console.log(date.toJSON())
+// console.log(date.toTimeString())
+
+const timeStamp = Date.now()
+// console.log(timeStamp)
+// console.log(Math.floor(timeStamp/1000));
+
+let newDate = new Date()
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+// console.log(newDate.getMonth() + 1);
+
