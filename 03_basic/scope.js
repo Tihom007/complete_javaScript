69e7db01e43058d3f0875d79aba342d7a86eb5d9
Tihom007 <130473417+Tihom007@users.noreply.github.com>
2024-02-12 16:01:33 +0000

    iife

--- 03_basic/scope.js
+++ 03_basic/scope.js
@@ -9,10 +9,77 @@ let a = 499;
     let a = 10;
     // const b = 20;
     var c = 30;
-    console.log("Inner" , a);
+    // console.log("Inner" , a);
 }
 
 // console.log(a); do not have access of a outside the block
 // console.log(b); do not have access of b outside block
-console.log("Outside" ,a);
-console.log(c);
+// console.log("Outside" ,a);
+// console.log(c);
+
+// nested scope
+
+function one(){
+    const userName = 'Tihom'
+
+
+    function two (){
+        const website = 'tihom.com'
+        // console.log(userName);
+    }
+    // console.log(website); will give error because website is not available outside function two
+
+    two();
+}
+
+one();
+
+if (true) {
+    const userName = 'tihom'
+
+    if(true){
+        let title = 'ramuk'
+        // console.log(userName + title);
+    }
+
+    // console.log(userName); error
+    
+}
+
+// console.log(title) error
+
+// function expression
+const addTwo = function (num){
+    return num + 2
+}
+
+addTwo(5) // hoisting is not supported in function expression
+
+// arrow function
+
+const gateUser = {
+    examName : 'Gate',
+    total_applicant: 198789,
+
+    examDetail: function (){
+        console.log(`${this.examName}, number of applicant ${this.total_applicant}`);
+    }
+}
+
+// gateUser.examDetail()
+// gateUser.total_applicant = 200000;
+// gateUser.examDetail()
+
+const arrowFun = () => {
+  let recentUser = 'Tihom'
+//   console.log(this.recentUser); o/p-> undefined
+} 
+
+arrowFun()
+
+// IIFE (Immediately Invoked function expression)
+
+(function print(){
+    console.log('IIFE Syntax');
+})();
+
