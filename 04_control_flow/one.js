6f2939de01997e583f545dcefc09235b49a2e575
Tihom007 <130473417+Tihom007@users.noreply.github.com>
2024-02-13 06:34:07 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,29 @@
+// if
+
+const isUserLoggedIn = true
+
+if(isUserLoggedIn){
+    console.log("Logged in");
+}
+else{
+    console.log("Not logged in");
+}
+
+// assignment (=) | loose equality (==) | strict equality(===) 
+
+// loose equality check value whereas strict equality check data type as well as value
+
+// switch case 
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+// falsy value --> false , 0 , "" , BigInt 0n, "", NaN, null, undefined
+
+//truthy value --> true, "0", "false", {}, [], function(){}
